f5cfdfd0cf1369d3d1861bb3a3a1bfb093a91be1
Imtiaz <axubair9@gmail.com>
2024-06-28 14:50:09 +0600

    variable done

---
+++ Semester One/Introduction to Programming in C Language/Variable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    /**
+        Definion: A variable is a fundamental concept in programming that acts as a storage location, identified by a name,
+        where data can be stored, retrieved, and manipulated. Variables are essential because they allow programmers to write
+        flexible and dynamic code that can handle different inputs and situations.
+    **/
+
+    // Example
+    int number = 10;
+
+    return 0;
+}
