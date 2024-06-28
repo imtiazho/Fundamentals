ee10372c082f05336071b31cee2cf839ab41756b
Imtiaz <axubair9@gmail.com>
2024-06-28 15:43:31 +0600

    Operator done

---
+++ Semester One/Introduction to Programming in C Language/Operators.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int main()
+{
+    /**
+        Definion: Operators in programming are special symbols or keywords used to perform operations on variables and values.
+        They are the building blocks of programming logic, allowing you to manipulate data and control the flow of a program.
+        Operators can be categorized into several types based on the operations they perform.
+        .
+        Arithmetic Operators given below
+        Operator	    Description	        Example
+        +	            Addition	        a + b
+        -	            Subtraction	        a - b
+        *	            Multiplication	    a * b
+        /	            Division	        a / b
+        %	            Modulus (remainder)	a % b
+        **	            Exponentiation	    a ** b
+        //	            Floor Division	    a // b
+        .
+        Comparison (Relational) Operators given below
+        Operator	Description 	            Example
+        ==	        Equal to	                a == b
+        !=	        Not equal to	            a != b
+        >	        Greater than	            a > b
+        <	        Less than	                a < b
+        >=	        Greater than or equal to    a >= b
+        <=	        Less than or equal to	    a <= b
+        .
+        Logical Operators given below
+        Operator	Description	    Example
+        &&	        Logical AND	    a && b
+        ||	        Logical OR	    a || b
+        !	        Logical NOT	    !a
+        .
+        Assignment Operators given below
+        Operator	Description	                Example
+        =	        Assign	                    a = b
+        +=	        Add and assign	            a += b
+        -=	        Subtract and assign	        a -= b
+        *=	        Multiply and assign	        a *= b
+        /=	        Divide and assign	        a /= b
+        %=	        Modulus and assign	        a %= b
+        **=	        Exponentiate and assign	    a **= b
+        //=	        Floor divide and assign	    a //= b
+        .
+        Bitwise Operators given below
+        Operator	Description	    Example
+        &	        Bitwise AND	    a & b
+        `	        `	            Bitwise OR
+        ^	        Bitwise XOR	    a ^ b
+        ~	        Bitwise NOT	    ~a
+        <<	        Left shift	    a << b
+        >>	        Right shift	    a >> b
+        .
+        Increment and decrement operators
+        Pre-Increment (++a) : Increments the value of a by 1, then returns the new value.
+        Post-Increment (a++): Returns the current value of a, then increments the value of a by 1.
+        Pre-Decrement (--b) : Decrements the value of b by 1, then returns the new value.
+        Post-Decrement (b--): Returns the current value of b, then decrements the value of b by 1.
+    **/
+
+
+
+    return 0;
+}
