9a5cf3fbfb50b522217ca68a10c643a605462e1e
Imtiaz <axubair9@gmail.com>
2024-07-02 23:18:56 +0600

    Basic data types

---
+++ Semester One/Problem Solving Part 1/2. Basic Data type.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    long long int lli;
+    char ch;
+    float fl;
+    double dou;
+
+    cin >> i >> lli >> ch >> fl >> dou;
+
+    cout << i << endl << lli << endl << ch << endl << fl << endl << dou;
+    return 0;
+}
