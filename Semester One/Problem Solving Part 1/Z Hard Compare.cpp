ad0dd11c340e5e66926f6b35c4adf89f40f45481
Imtiaz <axubair9@gmail.com>
2024-07-05 03:07:44 +0600

    Hard checker

---
+++ Semester One/Problem Solving Part 1/Z Hard Compare.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long A, B, C, D;
+    cin >> A >> B >> C >> D;
+
+    // Compute B * log(A) and D * log(C)
+    long double leftSide = B * log(A);
+    long double rightSide = D * log(C);
+
+    if (leftSide > rightSide) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
