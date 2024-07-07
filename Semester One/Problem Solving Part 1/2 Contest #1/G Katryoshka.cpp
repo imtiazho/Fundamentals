982f2d9cbee56cd3362e63f98e7880b494b1ad8b
Imtiaz <axubair9@gmail.com>
2024-07-07 18:36:32 +0600

    Making doll

--- Semester One/Problem Solving Part 1/2 Contest #1/G Katryoshka.cpp
+++ Semester One/Problem Solving Part 1/2 Contest #1/G Katryoshka.cpp
@@ -3,6 +3,43 @@
 using namespace std;
 
 int main() {
+    long long eye, mouth, body;
+    cin >> eye >> mouth >> body;
+    long long int doll = 0;
+    while (body != 0)
+    {
+        if (eye != 0 && mouth != 0)
+        {
+            if (mouth <= eye && mouth <= body)
+            {
+                doll += mouth;
+                body -= mouth;
+                eye -= mouth;
+                mouth = 0;
+            }
+            else if(eye <= mouth && eye <= body)
+            {
+                doll += eye;
+                body -= eye;
+                mouth -= eye;
+                eye = 0;
+            }
+        }
+        else
+        {
+            long long int setTwoEye = eye / 2;
+            if (setTwoEye >= body)
+            {
+                doll += body;
+            }
+            else
+            {
+                doll += setTwoEye;
+            }
+            break;
+        }
+    }
 
+    cout << doll;
     return 0;
 }
