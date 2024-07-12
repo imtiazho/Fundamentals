3ba62ac66999c848e53bc54979122983e4fe7c62
Imtiaz <axubair9@gmail.com>
2024-07-13 01:50:45 +0600

    shape2

--- Semester One/Problem Solving Part 1/3 Sheet #2 (Loops)/S Sum of Consecutive Odd Numbers.cpp
+++ Semester One/Problem Solving Part 1/3 Sheet #2 (Loops)/S Sum of Consecutive Odd Numbers.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main() {
+    int T;
+    cin >> T;
+    for(int i = 1; i <= T; i++)
+    {
+        int X, Y;
+        cin >> X >> Y;
 
+        int start = (X <= Y) ? X : Y;
+        int ending = (X <= Y) ? Y : X;
+
+        int sum = 0;
+        for(int j = start + 1; j < ending; j++)
+        {
+            if(j % 2 != 0)
+            {
+                sum += j;
+            }
+        }
+        cout << sum << endl;
+    }
     return 0;
 }
