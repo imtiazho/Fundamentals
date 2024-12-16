7383db32a73b35ca50e446cb2deddefea9d3cd6e
Imtiaz <axubair9@gmail.com>
2024-12-16 21:35:57 +0600

    Practice

---
+++ Semester Two/Basic Data Structures/1 Week/1 Time and Space  Complexity/Practice.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // Linear Complexity :: O(N)
+    // int N;
+    // cin >> N;  // O(1)
+    // for(int i = 1; i <= N; i++) // O(N)
+    // {
+    //     cout << i << " "; // O(1)
+    // }
+
+    // Logarithmic Complexity :: O(logN)
+    // int N;
+    // cin >> N;
+    // for(int i = 1; i <= N; i *= 2)
+    // {
+    //     cout << i << " ";
+    // }
+
+    // Square Root Complexity :: O(sqrt(N))
+    // int N;
+    // cin >> N;
+    // for(int i = 1; i*i <= N; i++)
+    // {
+    //     cout << i << " ";
+    // }
+
+    // Quadritic Complexity :: O(N^2)
+    // int N;
+    // cin >> N;
+    // for (int i = 1; i <= N; i++)
+    // {
+    //     for (int j = 1; j <= N; j++)
+    //     {
+
+    //         cout << i << " " << j << endl;
+    //     }
+    // }
+    // for (int i = 1; i <= N; i++) // O(N^3)
+    // {
+    //     for (int j = 1; j <= N; j++)
+    //     {
+    //         for (int k = 1; k <= N; k++)
+    //         {
+    //             cout << i << " " << j << " " << k << endl;
+    //         }
+    //     }
+    // }
+
+    // Linearothmic Complexity :: O(NlogN)
+    // int N;
+    // cin >> N;
+    // for (int i = 1; i <= N; i++)
+    // {
+    //     for (int j = 1; j <= N; j *= 2)
+    //     {
+    //         cout << i << " " << j << endl;
+    //     }
+    // }
+
+
+
+
+    // Space Complexity
+    // int N, sum = 0;
+    // cin >> N;
+    // for(int i = 1; i <= N; i++)
+    // {
+    //     sum += i;
+    // }
+    // cout << sum << endl;
+    // Its time complexity is O(1)
+
+    // Space Complexity
+    // int N;
+    // cin >> N;
+    // int Arr[N];
+    // for(int i = 0; i < N; i++)
+    // {
+    //     cin >> Arr[i];
+    // }
+    // Its complexity is O(N)
+
+    // int N;
+    // cin >> N;
+    // int Arr[N][N];
+    // for(int i = 0; i < N; i++)
+    // {
+    //     for(int j = 0; j < N; j++)
+    //     {
+    //         cin >> Arr[i][j];
+    //     }
+    // }
+    // Its time complexity is O(N^2)
+
+
+
+
+    // Problem List
+    /*
+        1. O(N^2)
+        2. O(N^2)
+        3. O(N * sqrt(N))
+        4. O(N^3)
+        5. O(sqrt(N))
+        6. O(NlogN)
+        7. O(N * NlogN)
+    */ 
+
+    return 0; // O(1)
+}
--- Semester Two/Basic Data Structures/1 Week/1 Time and Space  Complexity/Whole Module.cpp
+++ Semester Two/Basic Data Structures/1 Week/1 Time and Space  Complexity/Whole Module.cpp
@@ -1,8 +1,4 @@
 /*
-    Explanation:
-    https://docs.google.com/document/d/1HSGZzL_FK6rhXePW_79GzBuyWLe_wvObfAm9A8XZ5qU/edit?usp=sharing
-
-    Extra Practice Problem (Optional):
     https://docs.google.com/document/d/1i2zJTPCAImx0bPmCijqBeCP5-C_D-VVuJPdI68w-5RA/edit?usp=sharing
 */
 
