4baab67316ba2030c518d33941e4c7dfbf0ebe2e
Imtiaz Hossain <96795908+imtiazho@users.noreply.github.com>
2026-01-14 02:02:17 +0600

    Update Whole Module.cpp

--- Semester Two/Introduction to Algorithms/1 Week/2 Breadth First Search (BFS)/Whole Module.cpp
+++ Semester Two/Introduction to Algorithms/1 Week/2 Breadth First Search (BFS)/Whole Module.cpp
@@ -219,46 +219,82 @@ void bfs(int src)
     */
 
     /*
-        ||
+        || Path Printing 
+        vector<int> adjList[1005];
+        bool vis[1005];
+        bool level[1005];
+        int parent[1005];
+        
+        void bfs(int src)
+        {
+            queue<int> qu;
+            qu.push(src);
+            vis[src] = true;
+            level[src] = 0;
+        
+            while ((!qu.empty()))
+            {
+                // ber kore ana
+                int par = qu.front();
+                qu.pop();
+        
+                // Oree niye kaj koraa
+                cout << par << " ";
+        
+                // Children push kora
+                for (int child : adjList[par])
+                {
+                    if (vis[child] == false)
+                    {
+                        qu.push(child);
+                        vis[child] = true;
+                        level[child] = par + 1;
+                        parent[child] = par;
+                    }
+                }
+            }
+        }
+        .
+        .
+        int n, e;
+        cin >> n >> e;
+        vector<int> adjList[n];
+    
+        while (e--)
+        {
+            int a, b;
+            cin >> a >> b;
+            adjList[a].push_back(b);
+            adjList[b].push_back(a);
+        }
+    
+        memset(vis, false, sizeof(vis));
+        memset(level, -1, sizeof(level));
+        memset(parent, -1, sizeof(parent));
+        int src, dst;
+        cin >> src >> dst;
+        bfs(src);
+    
+        // for each parent
+        // for (int i = 0; i < n; i++)
+        // {
+        //     cout << i << " -> " << level[i] << endl;
+        // }
+    
+        vector<int> path;
+        int node = dst;
+        while (node == -1)
+        {
+            path.push_back(node);
+            node = parent[node];
+        }
+        reverse(path.begin(), path.end());
+        for (int x : path)
+        {
+            cout << x << " ";
+        }
+        
     */
-
-    int n, e;
-    cin >> n >> e;
-    vector<int> adjList[n];
-
-    while (e--)
-    {
-        int a, b;
-        cin >> a >> b;
-        adjList[a].push_back(b);
-        adjList[b].push_back(a);
-    }
-
-    memset(vis, false, sizeof(vis));
-    memset(level, -1, sizeof(level));
-    memset(parent, -1, sizeof(parent));
-    int src, dst;
-    cin >> src >> dst;
-    bfs(src);
-
-    // for each parent
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cout << i << " -> " << level[i] << endl;
-    // }
-
-    vector<int> path;
-    int node = dst;
-    while (node == -1)
-    {
-        path.push_back(node);
-        node = parent[node];
-    }
-    reverse(path.begin(), path.end());
-    for (int x : path)
-    {
-        cout << x << " ";
-    }
 
     return 0;
 }
