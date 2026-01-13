873b8624c75f2a52f223572c23e6aaa99dd82d6e
Imtiaz Hossain <96795908+imtiazho@users.noreply.github.com>
2026-01-14 03:10:02 +0600

    Update Whole Module.cpp

--- Semester Two/Introduction to Algorithms/1 Week/3 Depth First Search (BFS)/Whole Module.cpp
+++ Semester Two/Introduction to Algorithms/1 Week/3 Depth First Search (BFS)/Whole Module.cpp
@@ -8,48 +8,6 @@ bool vis[1005];
 bool visTwoD[105][105];
 vector<pair<int, int>> mov = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-void dfs(int src)
-{
-    // Base Case
-    // Auto off hoye jabee cause call korar agee amra check kore nicchi
-
-    // Print kore dea
-    cout << src << " ";
-    vis[src] = true;
-
-    // kaj kora
-    for (int child : adjList[src])
-    {
-        if (vis[child] == false)
-        {
-            dfs(child);
-        }
-    }
-}
-
-void dfsFor2D(int si, int sj)
-{
-    // Base Case
-    // Auto off hoye jabee cause call korar agee amra check kore nicchi
-
-    // Print kore dea
-    cout << si << " ";
-    visTwoD[si][sj] = true;
-
-    // kaj kora
-    for (int i = 0; i < 4; i++)
-    {
-        int ci, cj;
-        ci = si + mov[i].first;
-        cj = sj + mov[i].second;
-        cout << ci << " " << cj << endl;
-        // if (vis[child] == false)
-        // {
-        //     dfs(child);
-        // }
-    }
-}
-
 int main()
 {
     /*
@@ -91,21 +49,52 @@ int main()
     */
 
     /*
-        || DFS on 2D grid Implementation I
-    */
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> grid[i][j];
-        }
-    }
+        || DFS on 2D grid Implementation
+        char grid[105][105];
+        vector<int> adjList[1005];
+        bool vis[1005];
+        bool visTwoD[105][105];
+        vector<pair<int, int>> mov = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    int si, sj;
-    cin >> si >> sj;
-    dfsFor2D(si, sj);
+        void dfsFor2D(int si, int sj)
+        {
+            // Base Case
+            // Auto off hoye jabee cause call korar agee amra check kore nicchi
+        
+            // Print kore dea
+            cout << si << " ";
+            visTwoD[si][sj] = true;
+        
+            // kaj kora
+            for (int i = 0; i < 4; i++)
+            {
+                int ci, cj;
+                ci = si + mov[i].first;
+                cj = sj + mov[i].second;
+                cout << ci << " " << cj << endl;
+                // if (vis[child] == false)
+                // {
+                //     dfs(child);
+                // }
+            }
+        }
+        .
+        .
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> grid[i][j];
+            }
+        }
+    
+        int si, sj;
+        cin >> si >> sj;
+        dfsFor2D(si, sj);
+    */
+    
 
     return 0;
 }
