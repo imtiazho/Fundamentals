386d5073d074d6aac1f38b381e170ae4f0fb4d0b
Imtiaz <axubair9@gmail.com>
2026-01-24 05:00:19 +0600

    Practice multi-source bfs with new problem again

--- Semester Two/Introduction to Algorithms/2 Week/5 Practice/Random.cpp
+++ Semester Two/Introduction to Algorithms/2 Week/5 Practice/Random.cpp
@@ -204,3 +204,61 @@ public:
     }
 };
 */
+
+/*
+    class Solution {
+public:
+    vector<pair<int, int>> mov = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int n, m;
+
+    bool valid(int i, int j) {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+        return true;
+    }
+
+    void bfs(vector<vector<int>>& grid) {
+        queue<pair<int, int>> qu;
+        bool vis[n][m];
+        memset(vis, false, sizeof(vis));
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    vis[i][j] = true;
+                    qu.push({i, j});
+                }
+            }
+        
+        while(!qu.empty())
+        {
+            pair<int, int> par = qu.front();
+            int parI = par.first;
+            int parJ = par.second;
+            qu.pop();
+
+            for(int i = 0; i < 4; i++)
+            {
+                int ci, cj;
+                ci = parI + mov[i].first;
+                cj = parJ + mov[i].second;
+
+                if(valid(ci, cj) && (!vis[ci][cj] || grid[ci][cj] > grid[parI][parJ] + 1))
+                {
+                    qu.push({ci, cj});
+                    vis[ci][cj] = true;
+                    grid[ci][cj] = grid[parI][parJ] + 1;
+                }
+            }
+        }
+    }
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        n = mat.size();
+        m = mat[0].size();
+        bfs(mat);
+
+        return mat;
+    }
+};
+*/
