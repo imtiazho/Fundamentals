319de09b88c983dc84a1f380e45ddbe4c4233613
Imtiaz <axubair9@gmail.com>
2026-01-24 04:38:00 +0600

    Practice multi-source bfs

--- Semester Two/Introduction to Algorithms/2 Week/5 Practice/Random.cpp
+++ Semester Two/Introduction to Algorithms/2 Week/5 Practice/Random.cpp
@@ -144,3 +144,63 @@ public:
     }
 };
 */
+
+/*
+    class Solution {
+public:
+    vector<pair<int, int>> mov = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    bool vis[1005][1005];
+    int n, m;
+
+    bool valid(int i, int j) {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+        return true;
+    }
+
+    void bfs(vector<vector<int>>& grid) {
+        queue<pair<int, int>> qu;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 0;
+                    vis[i][j] = true;
+                    qu.push({i, j});
+                }
+            }
+        }
+
+        while(!qu.empty())
+        {
+            pair<int, int> par = qu.front();
+            int parI = par.first;
+            int parJ = par.second;
+            qu.pop();
+
+            for(int i = 0; i < 4; i++)
+            {
+                int ci, cj;
+                ci = parI + mov[i].first;
+                cj = parJ + mov[i].second;
+
+                if(valid(ci, cj) && !vis[ci][cj])
+                {
+                    qu.push({ci, cj});
+                    grid[ci][cj] = grid[parI][parJ] + 1;
+                    vis[ci][cj] = true; 
+                }
+            }
+        }
+    }
+
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        n = isWater.size();
+        m = isWater[0].size();
+
+        memset(vis, false, sizeof(vis));
+        bfs(isWater);
+
+        return isWater;
+    }
+};
+*/
