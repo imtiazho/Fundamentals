19957999a2171ad25efd0b387bae671cc1d73ef3
Imtiaz <axubair9@gmail.com>
2026-01-24 04:05:03 +0600

    Practice min score of the path between two path

--- Semester Two/Introduction to Algorithms/2 Week/5 Practice/Random.cpp
+++ Semester Two/Introduction to Algorithms/2 Week/5 Practice/Random.cpp
@@ -108,3 +108,39 @@ public:
     }
 };
 */
+
+/*
+    class Solution {
+public:
+    vector<pair<int, int>> adjList[100005];
+    bool vis[100005];
+    int mini = INT_MAX;
+
+    void dfs(int src)
+    {
+        vis[src] = true;
+
+        for(auto child : adjList[src])
+        {
+            mini = min(mini, child.second);
+            if(!vis[child.first])
+            {
+                dfs(child.first);
+            }
+        }
+    }
+
+    int minScore(int n, vector<vector<int>>& roads) {
+        for(auto road : roads)
+        {
+            adjList[road[0]].push_back({road[1], road[2]});
+            adjList[road[1]].push_back({road[0], road[2]});
+        }
+
+        memset(vis, false, sizeof(vis));
+        dfs(1);
+
+        return mini;
+    }
+};
+*/
