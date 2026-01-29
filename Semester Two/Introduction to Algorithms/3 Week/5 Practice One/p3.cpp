72d05dd2685adcfd70e58f557036d1ba1e0ddc42
Imtiaz <axubair9@gmail.com>
2026-01-29 06:54:14 +0600

    Solve a problem with dijkstra in optimizing way

--- Semester Two/Introduction to Algorithms/3 Week/5 Practice One/p3.cpp
+++ Semester Two/Introduction to Algorithms/3 Week/5 Practice One/p3.cpp
@@ -1,40 +1,67 @@
+/*
+    https://codeforces.com/problemset/problem/20/C
+*/
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 #define ll long long int
 
-vector<pair<ll, ll>> adjList[100005];
-ll dis[100005];
+vector<pair<ll, ll>> egdeList[100005];
 ll parent[100005];
+ll dis[100005];
 
-void dijkstra(ll src)
+void dijkstra(int src)
 {
     priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> qu;
-    dis[src] = 0;
     qu.push({0, src});
+    dis[src] = 0;
 
     while (!qu.empty())
     {
-        pair<ll, ll> par = qu.top();
-        ll pardis = par.first;
-        ll parNode = par.second;
+        auto par = qu.top();
+        int parDis = par.first;
+        int parNode = par.second;
         qu.pop();
 
-        if (pardis > dis[parNode])
-            continue;
+        if (parDis > dis[parNode]) continue;
 
-        for (auto child : adjList[parNode])
+        for (auto child : egdeList[parNode])
         {
-            ll childNode = child.first;
-            ll childDis = child.second;
+            int childNode = child.first;
+            int childDis = child.second;
 
-            if (pardis + childDis < dis[childNode])
+            if (parDis + childDis < dis[childNode])
             {
-                dis[childNode] = pardis + childDis;
+                dis[childNode] = parDis + childDis;
                 qu.push({dis[childNode], childNode});
                 parent[childNode] = parNode;
             }
+        }
+    }
+}
+
+void pathPrinting(ll n)
+{
+    if (dis[n] == LLONG_MAX)
+    {
+        cout << -1 << endl;
+    }
+    else{
+        vector<ll> path;
+        ll node = n;
+        while(node != -1)
+        {
+            path.push_back(node);
+            node = parent[node];
+        }
+
+        reverse(path.begin(), path.end());
+        for(ll x : path)
+        {
+            cout << x << " ";
         }
     }
 }
@@ -47,37 +74,18 @@ int main()
     {
         ll a, b, c;
         cin >> a >> b >> c;
-        adjList[a].push_back({b, c});
-        adjList[b].push_back({a, c});
+        egdeList[a].push_back({b, c});
+        egdeList[b].push_back({a, c});
     }
 
-    for (ll i = 1; i <= n; i++){
+    for (ll i = 1; i <= n; i++)
+    {
         dis[i] = LLONG_MAX;
         parent[i] = -1;
     }
 
     dijkstra(1);
-
-    if (dis[n] == LLONG_MAX)
-    {
-        cout << -1 << endl;
-    }
-    else
-    {
-        vector<ll> path;
-        ll node = n;
-        while (node != -1)
-        {
-            path.push_back(node);
-            node = parent[node];
-        }
-
-        reverse(path.begin(), path.end());
-        for (ll x : path)
-        {
-            cout << x << " ";
-        }
-    }
+    pathPrinting(n);
 
     return 0;
 }
