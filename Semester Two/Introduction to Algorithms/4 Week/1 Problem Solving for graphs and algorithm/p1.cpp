3d80635aeb31380919e55d730e7c98e0499b128f
Imtiaz <axubair9@gmail.com>
2026-01-31 07:56:55 +0600

    Solve a problem from module

--- Semester Two/Introduction to Algorithms/4 Week/1 Problem Solving for graphs and algorithm/p1.cpp
+++ Semester Two/Introduction to Algorithms/4 Week/1 Problem Solving for graphs and algorithm/p1.cpp
@@ -3,8 +3,70 @@
 
 using namespace std;
 
+int parent[100005];
+int groupSize[100005];
+int cmp, mx;
+
+int find(int node)
+{
+    cout << node << endl;
+    if (parent[node] == -1)
+    {
+        return node;
+    }
+
+    int leader = find(parent[node]);
+    parent[node] = leader;
+
+    return leader;
+}
+
+void dsuUnion(int node1, int node2)
+{
+    int leder1 = find(node1);
+    int leder2 = find(node2);
+    if(leder1 == leder2)
+    {
+        return;
+    }
+    if (groupSize[leder1] >= groupSize[leder2])
+    {
+        parent[leder2] = leder1;
+        groupSize[leder1] += groupSize[leder2];
+        mx = max(mx, groupSize[leder1]);
+    }
+    else
+    {
+        parent[leder1] = leder2;
+        groupSize[leder2] += groupSize[leder1];
+        mx = max(mx, groupSize[leder2]);
+    }
+
+    cmp--;
+}
+
 int main()
 {
-    
+
+    int n, e;
+    cin >> n >> e;
+    cmp = n;
+    mx = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = -1;
+        groupSize[i] = 1;
+    }
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        dsuUnion(a, b);
+        cout << cmp << " " << mx << endl;
+    }
+
     return 0;
 }
