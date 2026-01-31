dc885733f3c209c6612dbd8d568567ddc13249ff
Imtiaz <axubair9@gmail.com>
2026-01-31 22:07:50 +0600

    Solved a problem and took note for dynamic programming and memoization

--- Semester Two/Introduction to Algorithms/4 Week/2 Basic of Dynamic Programming/Whole Module.cpp
+++ Semester Two/Introduction to Algorithms/4 Week/2 Basic of Dynamic Programming/Whole Module.cpp
@@ -11,16 +11,47 @@ int fibo(long long int n)
 
     if (dp[n] != -1)
     {
-        dp[n] = fibo(n - 1) + fibo(n - 2);
+        return dp[n];
     }
 
+    dp[n] = fibo(n - 1) + fibo(n - 2);
     return dp[n];
 }
 
 int main()
 {
     /*
-        || Fibonacci with DP + Optimising Fibonacci :: top down
+        || Normal Fact
+        int fact(int n)
+        {
+            if(n == 1) return 1;
+
+            int mul = fact(n - 1);
+
+            return n * mul;
+        }
+
+    */
+
+    /*
+        || Fibonacci
+        int fibo(long long int n)
+        {
+            // if (n == 0)
+            //     return 0;
+            // else if(n == 1)
+            //     return 1;
+            if (n == 0 || n == 1)
+                return n;
+
+            if (n < 2)
+                return n;
+
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    */
+    /*
+        || Fibonacci with DP + Optimising Fibonacci
         .
         long long int dp[1005];
 
@@ -31,9 +62,35 @@ int main()
 
             if (dp[n] != -1)
             {
-                dp[n] = fibo(n - 1) + fibo(n - 2);
+                return dp[n];
             }
 
+            dp[n] = fibo(n - 1) + fibo(n - 2);
+            return dp[n];
+        }
+        .
+        memset(dp, -1, sizeof(dp));
+        long long int n;
+        cin >> n;
+        cout << fibo(n);
+    */
+
+    /*
+        || Fibonacci with DP + Optimising Fibonacci :: top down it use recursion
+        .
+        long long int dp[1005];
+
+        int fibo(long long int n)
+        {
+            if (n < 2)
+                return n;
+
+            if (dp[n] != -1)
+            {
+                return dp[n];
+            }
+
+            dp[n] = fibo(n - 1) + fibo(n - 2);
             return dp[n];
         }
         .
@@ -50,18 +107,18 @@ int main()
         int fibo[n + 1];
         fibo[0] = 0;
         fibo[1] = 1;
-    
-        for (int i = 0; i <= n; i++)
+
+        for (int i = 2; i <= n; i++)
         {
             fibo[i] = fibo[n - 1] + fibo[n - 2];
         }
-    
+
         cout << fibo[n] << endl;
     */
 
     /*
         || LeetCode Climbing Stairs
     */
-   
+
     return 0;
 }
