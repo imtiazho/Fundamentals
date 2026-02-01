b3338a7ac0837d07a5ccc0cdc323d57977724d72
Imtiaz <axubair9@gmail.com>
2026-02-01 23:53:20 +0600

    Solved leetcode problem to find fibonacci number with memoization

---
+++ Semester Two/Introduction to Algorithms/4 Week/4 Practice/4.cpp
@@ -0,0 +1,22 @@
+/*
+    || https://leetcode.com/problems/fibonacci-number/description/
+*/
+
+// class Solution {
+// public:
+//     long long int dp[35];
+
+//     int fibo(int n)
+//     {
+//         if(n < 2) return n;
+
+//         if(dp[n] != -1) return dp[n];
+//         return dp[n] = fibo(n - 1) + fibo(n - 2);
+//     }
+
+//     int fib(int n) {
+//         memset(dp, -1, sizeof(dp));
+
+//         return fibo(n);
+//     }
+// };
