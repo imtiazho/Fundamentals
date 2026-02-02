200354a163eee006651f1123a63d61a642119940
Imtiaz <axubair9@gmail.com>
2026-02-02 09:42:00 +0600

    Solved again that LC min sum in top down approach

--- Semester Two/Introduction to Algorithms/4 Week/5 Practice/3.cpp
+++ Semester Two/Introduction to Algorithms/4 Week/5 Practice/3.cpp
@@ -2,6 +2,7 @@
     || https://leetcode.com/problems/minimum-path-sum/description/
 */
 
+// Bottom Up Approach
 // class Solution {
 // public:
 //     int dp[205][205];
@@ -29,5 +30,40 @@
 //         memset(dp, -1, sizeof(dp));
 
 //         return find(0, 0, grid);
+//     }
+// };
+
+// Top Down Approach
+// class Solution {
+// public:
+//     int dp[205][205];
+//     int n, m;
+
+//     int find(int i, int j, vector<vector<int>>& grid)
+//     {
+//         if(i == 0 && j == 0) return grid[i][j];
+
+//         if(i < 0 || j < 0) return 1e9;
+
+//         // Apply DP
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int right = grid[i][j] + find(i, j - 1, grid);
+//         int down = grid[i][j] + find(i - 1, j, grid);
+        
+//         dp[i][j] =  min(right, down);
+        
+//         return dp[i][j];
+
+
+//     }
+
+//     int minPathSum(vector<vector<int>>& grid) {
+//         n = grid.size();
+//         m = grid[0].size();
+
+//         memset(dp, -1, sizeof(dp));
+
+//         return find(n - 1, m - 1, grid);
 //     }
 // };
