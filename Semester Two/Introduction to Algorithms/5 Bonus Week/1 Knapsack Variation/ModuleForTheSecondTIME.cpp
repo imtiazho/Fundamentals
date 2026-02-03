1ed5da85e7a055e9599762fb6ae624fb36c09ca1
Imtiaz <axubair9@gmail.com>
2026-02-03 08:48:37 +0600

    Revision and implimentaion unbound knapsack theory

--- Semester Two/Introduction to Algorithms/5 Bonus Week/1 Knapsack Variation/ModuleForTheSecondTIME.cpp
+++ Semester Two/Introduction to Algorithms/5 Bonus Week/1 Knapsack Variation/ModuleForTheSecondTIME.cpp
@@ -2,65 +2,61 @@
 
 using namespace std;
 
-int val[1005];
+int val[1005], weight[1005];
 int dp[1005][1005];
 
-bool subSetSum(int i, int sum)
+int knapsack(int i, int mxW)
 {
-    if (i < 0)
+    if (i < 0 || mxW <= 0)
     {
-        if (sum == 0)
-            return true;
-        else
-            return false;
+        return 0;
     }
 
-    if (dp[i][sum] != -1)
-        return dp[i][sum];
-
-    if (val[i] <= sum)
+    // DP Apply
+    if (dp[i][mxW] != -1)
     {
-        bool op1 = subSetSum(i - 1, sum - val[i]);
-        bool op2 = subSetSum(i - 1, sum);
+        return dp[i][mxW];
+    }
 
-        return dp[i][sum] = op1 || op2;
+    if (weight[i] <= mxW)
+    {
+        // Two options
+        // 1 bag rakhbo, 2 bag e rakhbo na!
+        int op1 = knapsack(i, mxW - weight[i]) + val[i];
+        int op2 = knapsack(i - 1, mxW);
+
+        // return max(op1, op2);
+        dp[i][mxW] = max(op1, op2);
+
+        return dp[i][mxW];
     }
     else
     {
-        return dp[i][sum] = subSetSum(i - 1, sum);
+        // One Options
+        // Bag e rakhte parbo na
+        dp[i][mxW] = knapsack(i - 1, mxW);
+        return dp[i][mxW];
     }
 }
 
 int main()
 {
-    int n;
-    int sum = 0;
+    int n, mxW;
     cin >> n;
+
     for (int i = 0; i < n; i++)
-    {
         cin >> val[i];
-        sum += val[i];
-    }
 
-    if (sum % 2 == 1)
-    {
-        cout << "NO" << endl;
-    }
-    else
-    {
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= sum; j++)
-                dp[i][j] = -1;
+    for (int i = 0; i < n; i++)
+        cin >> weight[i];
 
-        if (subSetSum(n - 1, sum / 2))
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "FALSE" << endl;
-        }
-    }
+    cin >> mxW;
+
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= mxW; j++)
+            dp[i][j] = -1;
+
+    cout << knapsack(n - 1, mxW) << endl;
 
     return 0;
 }
