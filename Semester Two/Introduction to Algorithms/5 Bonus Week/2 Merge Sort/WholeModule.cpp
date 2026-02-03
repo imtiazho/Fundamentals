68cbcb55c1decdc5d2397536d691008b7fbd8f54
Imtiaz <axubair9@gmail.com>
2026-02-04 02:27:44 +0600

    Combine two part Divide and conquer to impliment Merge Sort

--- Semester Two/Introduction to Algorithms/5 Bonus Week/2 Merge Sort/WholeModule.cpp
+++ Semester Two/Introduction to Algorithms/5 Bonus Week/2 Merge Sort/WholeModule.cpp
@@ -2,21 +2,68 @@
 
 using namespace std;
 
+void conquer(int c[], int l, int mid, int r)
+{
+    int n = mid - l + 1; // mid - l + 1
+    int a[n];
+    int k = l; // to keep track of params array index
+    for (int i = 0; i < n; i++, k++)
+    {
+        a[i] = c[k];
+        // k++; // you can use increament in here or init section
+    }
+
+    int m = r - mid;
+    int b[m];
+    k = mid + 1; // to keep track of params array index
+    for (int i = 0; i < m; i++, k++)
+    {
+        b[i] = c[k];
+        // z++ // you can use increament in here or init section
+    }
+
+    int i = 0, j = 0, curr = l;
+    while (i < n && j < m)
+    {
+        if (a[i] < b[j])
+        {
+            c[curr] = a[i];
+            i++;
+            curr++;
+        }
+        else
+        {
+            c[curr] = b[j];
+            j++;
+            curr++;
+        }
+    }
+
+    while (i < n)
+    {
+        c[curr] = a[i];
+        i++;
+        curr++;
+    }
+    while (j < m)
+    {
+        c[curr] = b[j];
+        j++;
+        curr++;
+    }
+}
+
 void divide(int a[], int l, int r)
 {
-    // To check how divide work
-    for (int i = l; i <= r; i++)
-    {
-        cout << a[i] << " ";
-    }
-    cout << endl;
-
     if (l >= r)
         return;
-    
+
     int mid = (l + r) / 2;
     divide(a, l, mid);
     divide(a, mid + 1, r);
+
+    // Call for conquer
+    conquer(a, l, mid, r);
 }
 
 int main()
@@ -31,11 +78,16 @@ int main()
 
     divide(a, 0, n - 1);
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
     return 0;
 }
 
 /*
-    || Conquer Implimentations
+    || Conquer Implimentations of merge sort
     #include <bits/stdc++.h>
 
     using namespace std;
@@ -100,4 +152,133 @@ int main()
     }
 */
 
+/*
+    || Divide Implimentations of merge sort
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    void divide(int a[], int l, int r)
+    {
+        // To check how divide work
+        for (int i = l; i <= r; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+
+        if (l >= r)
+            return;
+
+        int mid = (l + r) / 2;
+        divide(a, l, mid);
+        divide(a, mid + 1, r);
+    }
+
+    int main()
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        divide(a, 0, n - 1);
+
+        return 0;
+    }
+*/
+
+/*
+    || Total merge sort. Its total complexity is O(N*logN)
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    void conquer(int c[], int l, int mid, int r)
+    {
+        int n = mid - l + 1; // mid - l + 1
+        int a[n];
+        int k = l; // to keep track of params array index
+        for (int i = 0; i < n; i++, k++)
+        {
+            a[i] = c[k];
+            // k++; // you can use increament in here or init section
+        }
+
+        int m = r - mid;
+        int b[m];
+        k = mid + 1; // to keep track of params array index
+        for (int i = 0; i < m; i++, k++)
+        {
+            b[i] = c[k];
+            // z++ // you can use increament in here or init section
+        }
+
+        int i = 0, j = 0, curr = l;
+        while (i < n && j < m)
+        {
+            if (a[i] < b[j])
+            {
+                c[curr] = a[i];
+                i++;
+                curr++;
+            }
+            else
+            {
+                c[curr] = b[j];
+                j++;
+                curr++;
+            }
+        }
+
+        while (i < n)
+        {
+            c[curr] = a[i];
+            i++;
+            curr++;
+        }
+        while (j < m)
+        {
+            c[curr] = b[j];
+            j++;
+            curr++;
+        }
+    }
+
+    void divide(int a[], int l, int r)
+    {
+        if (l >= r)
+            return;
+
+        int mid = (l + r) / 2;
+        divide(a, l, mid);
+        divide(a, mid + 1, r);
+
+        // Call for conquer
+        conquer(a, l, mid, r);
+    }
+
+    int main()
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        divide(a, 0, n - 1);
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+
+        return 0;
+    }
+*/
 // ..
