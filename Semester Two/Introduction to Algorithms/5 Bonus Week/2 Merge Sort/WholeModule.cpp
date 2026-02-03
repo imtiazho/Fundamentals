67a5c3c1492e15310e61e78f19cf1212e0403c61
Imtiaz <axubair9@gmail.com>
2026-02-04 00:58:31 +0600

    Divide part Implementation of merge sort

--- Semester Two/Introduction to Algorithms/5 Bonus Week/2 Merge Sort/WholeModule.cpp
+++ Semester Two/Introduction to Algorithms/5 Bonus Week/2 Merge Sort/WholeModule.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+void divide(int a[], int l, int r)
+{
+    // To check how divide work
+    for (int i = l; i <= r; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    if (l >= r)
+        return;
+    
+    int mid = (l + r) / 2;
+    divide(a, l, mid);
+    divide(a, mid + 1, r);
+}
+
 int main()
 {
     int n;
@@ -12,51 +29,75 @@ int main()
         cin >> a[i];
     }
 
-    int m;
-    cin >> m;
-    int b[m];
-    for (int i = 0; i < m; i++)
-    {
-        cin >> b[i];
-    }
+    divide(a, 0, n - 1);
 
-    int c[n + m];
-    int i = 0, j = 0, curr = 0;
-    while (i < n && j < m)
+    return 0;
+}
+
+/*
+    || Conquer Implimentations
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    int main()
     {
-        if (a[i] < b[j])   
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        int m;
+        cin >> m;
+        int b[m];
+        for (int i = 0; i < m; i++)
+        {
+            cin >> b[i];
+        }
+
+        int c[n + m];
+        int i = 0, j = 0, curr = 0;
+        while (i < n && j < m)
+        {
+            if (a[i] < b[j])
+            {
+                c[curr] = a[i];
+                i++;
+                curr++;
+            }
+            else
+            {
+                c[curr] = b[j];
+                j++;
+                curr++;
+            }
+        }
+
+        while (i < n)
         {
             c[curr] = a[i];
             i++;
             curr++;
         }
-        else
+        while (j < m)
         {
             c[curr] = b[j];
             j++;
             curr++;
         }
+
+        for (int k = 0; k < n + m; k++)
+        {
+            cout << c[k] << " ";
+        }
+
+
+
+        return 0;
     }
+*/
 
-    while (i < n)
-    {
-        c[curr] = a[i];
-        i++;
-        curr++;
-    }
-    while (j < m)
-    {
-        c[curr] = b[j];
-        j++;
-        curr++;
-    }
-
-    for (int k = 0; k < n + m; k++)
-    {
-        cout << c[k] << " ";
-    }
-
-
-
-    return 0;
-}
+// ..
