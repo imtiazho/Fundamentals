841af23f7ec1b912f94bcf452213b01e497753e2
Imtiaz <axubair9@gmail.com>
2026-02-05 04:36:30 +0600

    Apply DP on finding subset sum

--- Semester Two/Introduction to Algorithms/5 Bonus Week/1 Knapsack Variation/PracticeWholeModule.cpp
+++ Semester Two/Introduction to Algorithms/5 Bonus Week/1 Knapsack Variation/PracticeWholeModule.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int val[105];
+int dp[105][105];
 
 bool findSubsetSum(int i, int sum)
 {
@@ -11,13 +12,16 @@ bool findSubsetSum(int i, int sum)
         return sum == 0;
     }
 
+    if (dp[i][sum] != -1)
+        return dp[i][sum];
+
     if (val[i] <= sum)
     {
-        return findSubsetSum(i - 1, sum - val[i]) || findSubsetSum(i - 1, sum);
+        return dp[i][sum] = findSubsetSum(i - 1, sum - val[i]) || findSubsetSum(i - 1, sum);
     }
     else
     {
-        return findSubsetSum(i - 1, sum);
+        return dp[i][sum] = findSubsetSum(i - 1, sum);
     }
 }
 
@@ -38,6 +42,60 @@ int main()
     {
         cout << "NO" << endl;
     }
-    
+
     return 0;
 }
+
+/*
+    || Subset Sum || The subset sum problem is a foundational decision problem in computer science and mathematics that determines if any combination of integers from a given set equals a specific target sum
+
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    int val[105];
+    int dp[105][105];
+
+    bool findSubsetSum(int i, int sum)
+    {
+        if (i < 0)
+        {
+            return sum == 0;
+        }
+
+        if (dp[i][sum] != -1)
+            return dp[i][sum];
+
+        if (val[i] <= sum)
+        {
+            return dp[i][sum] = findSubsetSum(i - 1, sum - val[i]) || findSubsetSum(i - 1, sum);
+        }
+        else
+        {
+            return dp[i][sum] = findSubsetSum(i - 1, sum);
+        }
+    }
+
+    int main()
+    {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+            cin >> val[i];
+
+        int sum;
+        cin >> sum;
+        if (findSubsetSum(n - 1, sum))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+
+        return 0;
+    }
+*/
+
+// ...
