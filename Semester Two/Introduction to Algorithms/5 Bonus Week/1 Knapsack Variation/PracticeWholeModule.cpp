ebf4bf3be5b2308a6b79355d1c50cbb41aefff58
Imtiaz <axubair9@gmail.com>
2026-02-05 08:09:37 +0600

    Combine two part and impliment Merge Sort

--- Semester Two/Introduction to Algorithms/5 Bonus Week/1 Knapsack Variation/PracticeWholeModule.cpp
+++ Semester Two/Introduction to Algorithms/5 Bonus Week/1 Knapsack Variation/PracticeWholeModule.cpp
@@ -2,35 +2,85 @@
 
 using namespace std;
 
-void divide(int a[], int l, int r)
+void conquer(int c[], int l, int mid, int r)
 {
-    for (int i = l; i <= r; i++)
+    int n = mid - l + 1;
+    int a[n];
+    int k = l;
+    for (int i = 0; i < n; i++, k++)
     {
-        cout << a[i] << " ";
+        a[i] = c[k];
     }
-    cout << endl;
 
+    int m = r - mid;
+    int b[m];
+    k = mid + 1;
+    for (int i = 0; i < m; i++, k++)
+    {
+        b[i] = c[k];
+    }
+
+    int i = 0, j = 0, curr = l;
+    while (i < n && j < m)
+    {
+        if (a[i] < b[j])
+        {
+            c[curr] = a[i];
+            curr++;
+            i++;
+        }
+        else
+        {
+            c[curr] = b[j];
+            curr++;
+            j++;
+        }
+    }
+
+    while (i < n)
+    {
+        c[curr] = a[i];
+        curr++;
+        i++;
+    }
+
+    while (j < m)
+    {
+        c[curr] = b[j];
+        curr++;
+        j++;
+    }
+}
+
+void mergeSort(int a[], int l, int r)
+{
     if (l >= r)
         return;
 
     int mid = (l + r) / 2;
+    mergeSort(a, l, mid);
+    mergeSort(a, mid + 1, r);
 
-    divide(a, l, mid);
-    divide(a, mid + 1, r);
+    conquer(a, l, mid, r);
 }
 
 int main()
 {
     int n;
     cin >> n;
-    int val[n];
+    int a[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> val[i];
+        cin >> a[i];
+    }
+
+    mergeSort(a, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
     }
     
-    divide(val, 0, n - 1);
-
     return 0;
 }
 
@@ -308,4 +358,96 @@ int main()
         return 0;
     }
 */
+
+/*
+    || Merge Sort
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    void conquer(int c[], int l, int mid, int r)
+    {
+        int n = mid - l + 1;
+        int a[n];
+        int k = l;
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = c[k];
+            k++;
+        }
+
+        int m = r - mid;
+        int b[m];
+        k = mid + 1;
+        for (int i = 0; i < m; i++)
+        {
+            b[i] = c[k];
+            k++;
+        }
+
+        int i = 0, j = 0, curr = l;
+        while (i < n && j < m)
+        {
+            if (a[i] < b[j])
+            {
+                c[curr] = a[i];
+                curr++;
+                i++;
+            }
+            else
+            {
+                c[curr] = b[j];
+                curr++;
+                j++;
+            }
+        }
+
+        while (i < n)
+        {
+            c[curr] = a[i];
+            i++;
+            curr++;
+        }
+
+        while (j < m)
+        {
+            c[curr] = b[j];
+            j++;
+            curr++;
+        }
+    }
+
+    void divide(int a[], int l, int r)
+    {
+        if (l >= r)
+            return;
+
+        int mid = (l + r) / 2;
+        divide(a, l, mid);
+        divide(a, mid + 1, r);
+
+        conquer(a, l, mid, r);
+    }
+
+    int main()
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        divide(a, 0, n - 1);
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+
+        return 0;
+    }
+*/
+
 // ...
