7922ca043212de1ec98780921f0166b1c79d5ebb
Imtiaz <axubair9@gmail.com>
2026-02-08 01:42:12 +0600

    Solved problem colourBlindness from codeforce

--- Semester Two/XPSC/1 Week/13.cpp
+++ Semester Two/XPSC/1 Week/13.cpp
@@ -8,41 +8,61 @@ using namespace std;
 
 int main()
 {
+    // Shorter Version with element changing
     int t;
     cin >> t;
     while (t--)
     {
         int n;
-        cin >> n;
-        int val[n];
-        for (int i = 0; i < n; i++)
-            cin >> val[i];
+        string s1, s2;
+        cin >> n >> s1 >> s2;
 
-        for (int i = 0; i < n; i++)
+        for(int i = 0; i < n; i++)
         {
-            int x;
-            string moves;
-            cin >> x >> moves;
-
-            for (char c : moves)
-            {
-                if (c == 'U')
-                {
-                    val[i] = val[i] == 0 ? 9 : val[i] - 1;
-                }
-                else if (c == 'D')
-                {
-                    val[i] = val[i] == 9 ? 0 : val[i] + 1;
-                }
-            }
+            if(s1[i] == 'G') s1[i] = 'B';
+            if(s2[i] == 'G') s2[i] = 'B';
         }
 
-        for (int x : val)
-        {
-            cout << x << " ";
-        }
-        cout << endl;
+        cout << (s1 == s2 ? "YES" : "NO") << endl; 
     }
 
     return 0;
 }
+
+
+/*
+    || First Soln
+    #include <bits/stdc++.h>
+
+    using namespace std;
+
+    int main()
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            int n;
+            cin >> n;
+            string s1, s2;
+            cin >> s1 >> s2;
+
+            bool flag = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (s1[i] != 'R' && s2[i] == 'R' || s2[i] != 'R' && s1[i] == 'R')
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag)
+                cout << "NO" << endl;
+            else
+                cout << "YES" << endl;
+        }
+
+        return 0;
+    }
+*/
