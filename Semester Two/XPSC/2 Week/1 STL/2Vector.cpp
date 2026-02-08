563b12a49c4f0b25d45f68b5282de04468bc02f7
Imtiaz <axubair9@gmail.com>
2026-02-08 10:33:35 +0600

    Note about Vector STL

--- Semester Two/XPSC/2 Week/1 STL/2Vector.cpp
+++ Semester Two/XPSC/2 Week/1 STL/2Vector.cpp
@@ -6,8 +6,12 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    /*
 
-    // vector<int> v(2);
+    */
+    // int n;
+    // cin >> n;
+    // // vector<int> v(n);
     // vector<int> v;
     // for (int i = 0; i < 2; i++)
     // {
@@ -17,9 +21,9 @@ int main()
     //     v.push_back(x);
     // }
 
-    // cout << v.size();
-    // // v.pop_back();
-    // cout << v.size();
+    // cout << v.size() << endl;
+    // v.pop_back();
+    // cout << v.size() << endl;
     // for (int i = 0; i < v.size(); i++)
     // {
     //     cout << v[i] << endl;
@@ -30,6 +34,8 @@ int main()
 
     // Check empty or not
     // cout << v.empty() << endl;
+    
+    // ....
 
     // vector<int> v(10);
     // vector<int> v(10, 5);
@@ -46,39 +52,55 @@ int main()
     // }
     // for (int i = 0; i < v.size(); i++)
     // {
-    //     cout << v[i] << endl;
+    //     cout << v[i] << " ";
     // }
 
-    // vector<int> v;
-    // for (int i = 0; i < v.size(); i++)
-    // {
-    //     cin >> v[i];
-    // }
+    // ... 
+
+    /* int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    
     // auto it = v.begin();
     // auto it = v.begin() + 1;
-    // cout << *it << endl;
-    // for (auto it = v.begin(); it != v.end(); it++)
-    // {
-    //     cout << *it << " ";
-    // }
+    // cout << *it  << endl;
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    } */
 
-    vector<int> v;
+    // ...
+
+    /* vector<int> v(3);
     for (int i = 0; i < v.size(); i++)
     {
         cin >> v[i];
     }
-    // auto lastElement = v.end();
+    // auto lastElement = --v.end();
     // lastElement--;
+    // // auto lastElement = --v.end();
     // cout << *lastElement << endl;
 
     // reverse(v.begin(), v.end());
     // sort( v.begin(), v.end());
-    auto mn = min_element(v.begin(), v.end());
-    auto mx = max_element(v.begin(), v.end());
+    // sort( v.rbegin(), v.rend());
+    // sort( v.rbegin(), v.rend(), greater<>()); */
+    
+    // ..
 
-    // how to find max element positon by iterator
-    int maxElementPosition = mx - v.begin();
-    cout << maxElementPosition << endl;
+    /* // auto mn = min_element(v.begin(), v.end());
+    auto mx = max_element(v.begin(), v.end());
+    // cout << *mx << " " << *mn << endl;
+
+    // // how to find max element positon by iterator
+    // int maxElementPosition = mx - v.begin();
+    // cout << maxElementPosition << endl; */
 
     return 0;
 }
