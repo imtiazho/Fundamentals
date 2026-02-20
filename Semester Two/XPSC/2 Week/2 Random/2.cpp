69097027d66d8d0d6eceab73178f4a0d3a3920b6
Imtiaz <axubair9@gmail.com>
2026-02-20 23:48:23 +0600

    Solved a problem Change Please from cc

--- Semester Two/XPSC/2 Week/2 Random/2.cpp
+++ Semester Two/XPSC/2 Week/2 Random/2.cpp
@@ -1,4 +1,4 @@
-// https://www.codechef.com/problems/HEATWAVE
+// https://codeforces.com/problemset/problem/2065/A
 
 #include <bits/stdc++.h>
 
---
+++ Semester Two/XPSC/2 Week/2 Random/3.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/CHANGE_PLZ
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int x;
+		cin >> x;
+		cout << ((100 - x) / 10) * 10 << endl;
+	}
+}
