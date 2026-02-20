1a10e67e2c44c25201cab49a9afa12525fb83782
Imtiaz <axubair9@gmail.com>
2026-02-20 23:32:26 +0600

    Solved a problem plural to singular

---
+++ Semester Two/XPSC/2 Week/2 Random/2.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/problems/HEATWAVE
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		string s;
+		cin >> s;
+		s.pop_back();
+		s.pop_back();
+		s.push_back('i');
+		cout << s << endl;
+	}
+}
